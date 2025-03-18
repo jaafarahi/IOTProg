04874b8d8fa90afd4f774467690a1e0249ab0ffd
jaafarahi <37632@ma-web.nl>
2025-03-18 15:35:49 +0100

    Add files via upload

---
+++ opd1.ino
@@ -0,0 +1,87 @@
+void setup() {
+  pinMode(D3, OUTPUT);
+  pinMode(D5, OUTPUT;
+  pinMode(D6, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(D3, HIGH);
+  delay(500);
+  digitalWrite(D3, LOW);
+  delay(250)
+
+    digitalWrite(D3, HIGH);
+  delay(1500);
+  digitalWrite(D3, LOW);
+  delay(250)
+
+      digitalWrite(D3, HIGH);
+  delay(1500);
+  digitalWrite(D3, LOW);
+  delay(250)
+
+      digitalWrite(D3, HIGH);
+  delay(1500);
+  digitalWrite(D3, LOW);
+  delay(250)
+
+      digitalWrite(D6, HIGH);
+  delay(500);
+  digitalWrite(D6, LOW);
+  delay(250)
+
+        digitalWrite(D6, HIGH);
+  delay(1500);
+  digitalWrite(D6, LOW);
+  delay(250)
+
+  digitalWrite(D6, HIGH);
+  delay(500);
+  digitalWrite(D6, LOW);
+  delay(250)
+
+  digitalWrite(D6, HIGH);
+  delay(1500);
+  digitalWrite(D6, LOW);
+  delay(250)
+
+  digitalWrite(D5, HIGH);
+  delay(500);
+  digitalWrite(D5, LOW);
+  delay(250)
+
+    digitalWrite(D5, HIGH);
+  delay(500);
+  digitalWrite(D5, LOW);
+  delay(250)
+
+    digitalWrite(D5, HIGH);
+  delay(1500);
+  digitalWrite(D5, LOW);
+  delay(250)
+
+  digitalWrite(D5, HIGH);
+  delay(500);
+  digitalWrite(D5, LOW);
+  delay(250)
+
+  digitalWrite(D6, HIGH);
+  delay(500);
+  digitalWrite(D6, LOW);
+  delay(250)
+
+  digitalWrite(D6, HIGH);
+  delay(1500);
+  digitalWrite(D6, LOW);
+  delay(250)
+
+  digitalWrite(D3, HIGH);
+  delay(500);
+  digitalWrite(D3, LOW);
+  delay(250)
+
+    digitalWrite(D3, HIGH);
+  delay(1500);
+  digitalWrite(D3, LOW);
+  delay(250)
+}
